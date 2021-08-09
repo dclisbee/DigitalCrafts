1529982e55403c53f79cfd209a3effb9c1122d19
dclisbee <dmclisbee@gmail.com>
2021-08-09 07:50:52 -0500

    Update TODO.py

--- Week 1/day4/TODO.py
+++ Week 1/day4/TODO.py
@@ -1,65 +1,62 @@
-tasks_high = {}
-tasks_med = {}
-tasks_low = {}
-all_tasks = []
-
-while True:
+todoList = []
+todoDict = {}
+input1 = ""
+while input1 != 'q':
     print("Press 1 to add task\n") 
     print("Press 2 to delete task\n")
     print("Press 3 to view all tasks\n") 
     print("Press q to quit\n")
-    input1 = input()
     
-    if input1 == "q":
-        break
+    input1 = input()
 
-    elif input1 == "1":
+    if input1 == "1":
+        
         task = input("What is the title of the task?\n")
         priority = input("What is the priority: low, med, high?\n")
-        if priority == "high":
-            tasks_high[task] = priority
-
-        elif priority == "low":
-            tasks_low[task] = priority
-
-        elif priority == "med":
-            tasks_med[task] = priority
-        
-        all_tasks = [tasks_high, tasks_med, tasks_low]
+        full_task = task + " " + priority
+        todoDict["title"] = task
+        todoDict["priority"] = priority
+        todoList.append(todoDict)
 
     elif input1 == "2":
-        count = 0
-        for item in all_tasks:
+        count = 1
+        for item in todoList:
             print(item, count)
             count += 1
+
         task_to_del = int(input("Which index would you like to delete?\n"))
-        del all_tasks[task_to_del]
+        taskToDeleteIndex = task_to_del - 1
+        del todoList[taskToDeleteIndex]
+        #three for loops like pulling info to baseoff input and remove individ
+        
         print("Task has been removed\n")
 
 
     elif input1 == "3":
         count = 1
-        for item in all_tasks:
+        for item in todoList:
+            # print(str(count) + " - " + item + " - ")
+            print(" %d, %s, %s " % (count, item["title"], item["priority"]))
+            count += 1
             
-            #print(" - " + item + " - ")
+            
+            
             #if statement not printing empty dicts
-            if item == {}:
-                pass
+            # if item == {}:
+                # pass
                 #print(count)
-        for key, value in tasks_high.items():
-                if True:
-                    print(count, ' - ', key, ' - ', value)
-                    count += 1
-        for key, value in tasks_med.items():
-                if True:  
-                    print(count, ' - ', key, ' - ', value)
-                    count += 1
-        for key, value in tasks_low.items():
-                if True:    
-                    print(count, ' - ', key, ' - ', value)
-                    count += 1
-                #print('\n'.join("{}: {}".format(k, v) for k, v in all_tasks.items()))
-            
-                
 
-            
+
+        # for key, value in tasks_high.items():
+        #         if True:
+        #             print(count, ' - ', key, ' - ', value)
+        #             count += 1
+        # for key, value in tasks_med.items():
+        #         if True:  
+        #             print(count, ' - ', key, ' - ', value)
+        #             count += 1
+        # for key, value in tasks_low.items():
+        #         if True:    
+        #             print(count, ' - ', key, ' - ', value)
+        #             count += 1
+                #print('\n'.join("{}: {}".format(k, v) for k, v in all_tasks.items()))
