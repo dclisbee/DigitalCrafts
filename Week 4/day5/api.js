23a94158c2411126fb631d8f150ba19c9d29a858
dclisbee <dmclisbee@gmail.com>
2021-08-27 15:40:50 -0500

    removed key

--- Week 4/day5/api.js
+++ Week 4/day5/api.js
@@ -1,20 +1,22 @@
 
-var inputVal = document.getElementById(".input").value;
-async function fetchData(inputVal2){
+
+async function fetchData(){
     apiKey = "ff1cadec65d9f10c3aafc2a01b14612c";
-    
-    url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal2}&appid=${apiKey}&units=metric`;
+    inputVal = document.getElementById(".input").value;
+    url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
     const data = await fetch(url);
     const jsonData = await data.json();
     console.log(JSON.stringify(jsonData));   
-    
+    alert(inputVal);
     return jsonData;
 }
 // let input = 
 const submitButton = document.querySelector(".submit")
-const form = document.querySelector(".top-banner form");
+const form = document.querySelector(".input");
 submitButton.addEventListener("click", (e) => {
     e.preventDefault();
-    fetchData(inputVal);
+    console.log();
+    fetchData();
+    
 });
     
