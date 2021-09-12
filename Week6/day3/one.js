c513e0a4001aa2e606042379e25cb352f19562bc
dclisbee <dmclisbee@gmail.com>
2021-09-12 16:22:23 -0500

    tweak

--- Week6/day3/one.js
+++ Week6/day3/one.js
@@ -4,14 +4,8 @@ const token = process.env.API_TOKEN;
 const URL = process.env.URL;
 const { createClient } = require("@supabase/supabase-js");
 const supabase = createClient(URL, token);
-
 const app = express();
-
-// middleware starts
 app.use(express.json());
-
-// middleware ends
-
 const PORT = 3001;
 
 app.post("/createStock", async (req, res) => {
